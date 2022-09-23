2d64142e5337cc44674cd309852999c22d267fd8
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-09-23 20:43:25 +0200

    Add apples

--- ultra_bevy/src/lib.rs
+++ ultra_bevy/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::type_complexity)]
+
 extern crate wee_alloc;
 
 #[global_allocator]
@@ -7,8 +9,10 @@ use bevy::math::ivec2;
 use bevy::prelude::*;
 use bevy_system_graph::SystemGraph;
 use derive_more::From;
-use iyes_loopless::prelude::IntoConditionalSystem;
+use iyes_loopless::prelude::{ConditionHelpers, IntoConditionalSystem};
 use once_cell::sync::Lazy;
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
 use send_wrapper::SendWrapper;
 use ultra_crustaceous::Color as UltraColor;
 use ultra_crustaceous::*;
@@ -78,7 +82,7 @@ pub fn update(p1: u8, p2: u8) {
 
 // above is boiler-plate
 
-#[derive(Component, Deref, DerefMut, Clone, Copy, From)]
+#[derive(Component, Deref, DerefMut, Clone, Copy, From, PartialEq, Eq)]
 struct TilePos(IVec2);
 
 #[derive(Component, Deref, DerefMut, Clone, Copy, From)]
@@ -90,14 +94,43 @@ struct SnakeHead;
 #[derive(Component)]
 struct SnakeBody(Entity);
 
-#[derive(Default, Deref, DerefMut)]
+#[derive(Deref, DerefMut)]
+struct Tail(Entity);
+
+#[derive(Component)]
+struct Apple;
+
+#[derive(Default, Deref, DerefMut, Clone, Copy)]
 struct Tick(usize);
+
+#[derive(Default)]
+struct Random(Option<SmallRng>);
+
+impl From<Tick> for SmallRng {
+    fn from(tick: Tick) -> Self {
+        SmallRng::seed_from_u64(*tick as u64)
+    }
+}
+
+#[derive(PartialEq, Eq)]
+enum State {
+    Running,
+    GameOver,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::Running
+    }
+}
 
 fn init() -> App {
     let mut app = App::new();
 
     app.add_plugin(UltraPlugin)
         .init_resource::<Tick>()
+        .init_resource::<Random>()
+        .init_resource::<State>()
         .add_startup_system(setup);
 
     let graph = SystemGraph::new();
@@ -105,12 +138,14 @@ fn init() -> App {
     // todo: clean up when stageless is implemented
     graph
         .root(tick)
-        .then(update_head_dir)
-        .then(move_head.run_if(on_step))
-        .then(move_body.run_if(on_step))
-        .then(update_body_dir.run_if(on_step))
+        .then(update_head_dir.run_if(alive))
+        .then(move_head.run_if(on_step).run_if(alive))
+        .then(move_body.run_if(on_step).run_if(alive))
+        .then(eat_apples.run_if(on_step).run_if(alive))
+        .then(update_body_dir.run_if(on_step).run_if(alive))
         .then(draw_background.run_if(on_step))
-        .then(draw_head.run_if(on_step));
+        .then(draw_apples.run_if(on_step))
+        .then(draw_snake.run_if(on_step));
 
     app.add_system_set(graph.into());
 
@@ -121,6 +156,23 @@ const TILE_SIZE: usize = 10;
 const MAP_SIZE: IVec2 = ivec2(25, 20);
 const TICKS_PER_STEP: usize = 5;
 
+#[derive(Bundle)]
+struct SnakeBodyBundle {
+    body: SnakeBody,
+    pos: TilePos,
+    dir: Direction,
+}
+
+impl SnakeBodyBundle {
+    pub fn new(pos: TilePos, next: Entity) -> Self {
+        Self {
+            body: SnakeBody(next),
+            pos,
+            dir: Direction(IVec2::ZERO),
+        }
+    }
+}
+
 fn setup(mut commands: Commands, mut palette: ResMut<PaletteBuffer>) {
     // TODO: convert from bevy colors?
     palette[0] = UltraColor::from_rgb(0x302c2e); // dark brown
@@ -129,19 +181,21 @@ fn setup(mut commands: Commands, mut palette: ResMut<PaletteBuffer>) {
     palette[3] = UltraColor::from_rgb(0x71aa34); // green
     palette[4] = UltraColor::from_rgb(0xa93b3b); // deep red
 
+    let start_pos = TilePos(MAP_SIZE / 2);
+
     let mut tail = commands
-        .spawn_bundle((SnakeHead, TilePos(MAP_SIZE / 2), Direction(IVec2::ZERO)))
+        .spawn_bundle((SnakeHead, start_pos, Direction(IVec2::ZERO)))
         .id();
 
     for _ in 0..5 {
         tail = commands
-            .spawn_bundle((
-                SnakeBody(tail),
-                TilePos(MAP_SIZE / 2),
-                Direction(IVec2::ZERO),
-            ))
+            .spawn_bundle(SnakeBodyBundle::new(start_pos, tail))
             .id();
     }
+
+    commands.insert_resource(Tail(tail));
+
+    commands.spawn_bundle((Apple, TilePos(ivec2(2, 2))));
 }
 
 fn tick(mut tick: ResMut<Tick>) {
@@ -150,6 +204,10 @@ fn tick(mut tick: ResMut<Tick>) {
 
 fn on_step(tick: Res<Tick>) -> bool {
     **tick % TICKS_PER_STEP == 0
+}
+
+fn alive(state: Res<State>) -> bool {
+    *state == State::Running
 }
 
 fn update_head_dir(mut heads: Query<&mut Direction, With<SnakeHead>>, input: Res<UltraInput>) {
@@ -170,16 +228,64 @@ fn update_head_dir(mut heads: Query<&mut Direction, With<SnakeHead>>, input: Res
 }
 
 // combine with move_body?
-fn move_head(mut heads: Query<(&mut TilePos, &Direction), With<SnakeHead>>) {
+fn move_head(
+    mut state: ResMut<State>,
+    mut heads: Query<(&mut TilePos, &Direction), With<SnakeHead>>,
+    body_parts: Query<&TilePos, (With<SnakeBody>, Without<SnakeHead>)>,
+) {
     for (mut pos, dir) in heads.iter_mut() {
+        if **dir == IVec2::ZERO {
+            continue;
+        }
+
         let new_pos = **pos + **dir;
-        **pos = new_pos;
+
+        let dead = new_pos.x < 0
+            || new_pos.y < 0
+            || new_pos.x >= MAP_SIZE.x
+            || new_pos.y >= MAP_SIZE.y
+            || body_parts.iter().any(|body_pos| **body_pos == new_pos);
+
+        if dead {
+            *state = State::GameOver;
+        } else {
+            **pos = new_pos;
+        }
     }
 }
 
 fn move_body(mut parts: Query<(&mut TilePos, &Direction), With<SnakeBody>>) {
     for (mut pos, dir) in parts.iter_mut() {
         **pos += **dir;
+    }
+}
+
+fn eat_apples(
+    mut commands: Commands,
+    heads: Query<&TilePos, (With<SnakeHead>, Without<Apple>)>,
+    body_positions: Query<&TilePos, Without<Apple>>,
+    mut apples: Query<&mut TilePos, With<Apple>>,
+    mut tail: ResMut<Tail>,
+    mut rng: ResMut<Random>,
+    tick: Res<Tick>,
+) {
+    for head_pos in heads.iter() {
+        for mut apple_pos in apples.iter_mut() {
+            if *apple_pos == *head_pos {
+                let tail_pos = body_positions.get(**tail).unwrap();
+
+                // spawn extra segments for worm on the tail
+                for _ in 0..5 {
+                    **tail = commands
+                        .spawn_bundle(SnakeBodyBundle::new(*tail_pos, **tail))
+                        .id();
+                }
+
+                // move the apple
+                let rng = rng.0.get_or_insert_with(|| SmallRng::from(*tick));
+                **apple_pos = ivec2(rng.gen_range(0..MAP_SIZE.x), rng.gen_range(0..MAP_SIZE.y));
+            }
+        }
     }
 }
 
@@ -194,17 +300,32 @@ fn update_body_dir(
     }
 }
 
-fn draw_head(heads: Query<&TilePos>, mut screen: ResMut<OutputBuffer>) {
-    for pos in heads.iter() {
-        draw_tile(&mut *screen, *pos, 3);
-    }
-}
-
 fn draw_background(mut screen: ResMut<OutputBuffer>) {
     for x in 0..MAP_SIZE.x {
         for y in 0..MAP_SIZE.y {
             draw_tile(&mut *screen, TilePos(ivec2(x, y)), 1);
         }
+    }
+}
+
+fn draw_apples(apples: Query<&TilePos, With<Apple>>, mut screen: ResMut<OutputBuffer>) {
+    for pos in apples.iter() {
+        draw_tile(&mut *screen, *pos, 4);
+    }
+}
+
+fn draw_snake(
+    state: Res<State>,
+    heads: Query<&TilePos, Or<(With<SnakeBody>, With<SnakeHead>)>>,
+    mut screen: ResMut<OutputBuffer>,
+) {
+    let color = match *state {
+        State::GameOver => 4,
+        _ => 3,
+    };
+
+    for pos in heads.iter() {
+        draw_tile(&mut *screen, *pos, color);
     }
 }
 
